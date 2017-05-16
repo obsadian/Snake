67b94feba085374b36983cfd0d63908027674fca
Rossana <rossana_naki@hotmail.com>
2017-05-16 08:27:31 +0100

    Codigo comentado apagado

--- Particle.js
+++ Particle.js
@@ -56,30 +56,6 @@ class ParticleSystem {
         for (var i = 0; i < numeroDeParticulas; i++) {
             if (particleIndex > 1000)
                 particleIndex = 0;
-
-            /*particles[particleIndex] = new Particula(jogo.width / 2, jogo.height / 2,
-                Math.random() * 10 - 5, Math.random() * 10 - 5,
-                ("hsla(" + parseInt(Math.random() * 360, 10) + ", 100%, 60%, 0.2)"), Math.random() * 30 + 10, 10, 10);*/
-            //jogo.contexto.fillStyle = corRGBA(0, 0, 0, Math.random());
-            //jogo.contexto.fillRect(0, 0, jogo.width, jogo.height);
-
-            /*particles[particleIndex] = new ParticulaCircular(jogo.width / 4, jogo.height / 2,
-                Math.random() * 10 - 5, Math.random() * 10 - 5,
-                corRGBA(rand(250, 180), rand(250, 180), rand(250, 180), Math.random()), Math.floor(Math.random() * 5), 5, 5);
-            particles[particleIndex].id = particleIndex;
-            particleIndex++;
-            particles[particleIndex] = new ParticulaCircular(jogo.width / 2, jogo.height / 2,
-                Math.random() * 10 - 5, Math.random() * 10 - 5,
-                corRGBA(rand(160, 10), rand(250, 180), rand(160, 10), Math.random()), Math.floor(Math.random() * 5), 5, 5);
-            particles[particleIndex].id = particleIndex;
-            particleIndex++;
-            particles[particleIndex] = new ParticulaCircular(jogo.width / 4 * 3, jogo.height / 2,
-                Math.random() * 10 - 5, Math.random() * 10 - 5,
-                corRGBA(rand(250, 180), rand(160, 10), rand(160, 10), Math.random()), Math.floor(Math.random() * 5), 5, 5);
-
-            particles[particleIndex].id = particleIndex;
-            particleIndex++;*/
-
             particles[particleIndex] = new ParticulaCircular(posX, posY, vx, vy, color, maxLife, largura, comprimento);
             particles[particleIndex].id = particleIndex;
             particleIndex++;
