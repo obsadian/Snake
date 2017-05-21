1cd5b4a567a6fdfa820135b543157e334da33b35
obsadian <obsadian@hotmail.com>
2017-05-21 04:02:01 +0100

    Add files via upload
    Criação de paredes e criação da cobra a mudar de forma.

---
+++ Particle.js
@@ -0,0 +1,115 @@
+//ver: https://www.youtube.com/watch?v=YCI8uqePkrc
+
+var particleIndex = 0;
+var particles = {};
+var numeroDeParticulas = 10;
+
+class Particula {
+    constructor(posX, posY, vx, vy, color, maxLife, radius) {
+        this.id = 0;
+        this.posX = posX;
+        this.posY = posY;
+        this.vx = vx;
+        this.vy = vy;
+        this.color = color;
+        this.maxLife = maxLife;
+        this.radius = radius;
+
+        this.gravity = 0;
+        this.life = 0;
+    }
+
+    draw() {
+        this.posX += this.vx; // positivo: direita; negativo: esquerda
+        this.posY += this.vy; // positivo: baixo; negativo: cima
+        this.vy += this.gravity;
+        this.life++;
+        if (this.life >= this.maxLife)
+            delete particles[this.id];
+        //        jogo.contexto.beginPath();
+        //        jogo.contexto.arc(posX, posY, radius, 0, 2 * Math.PI, false);
+        jogo.contexto.fillStyle = this.color;
+        jogo.contexto.fillRect(this.posX, this.posY, this.radius, this.radius);
+        //       jogo.contexto.fill();
+    }
+}
+
+class ParticulaCircular extends Particula {
+    draw() {
+        this.posX += this.vx; // positivo: direita; negativo: esquerda
+        this.posY += this.vy; // positivo: baixo; negativo: cima
+
+
+        this.vx = Math.random() * 2;
+        this.vy = Math.random() * 2;
+
+
+        this.life++;
+        if (this.life >= this.maxLife)
+            delete particles[this.id];
+        jogo.contexto.beginPath();
+        jogo.contexto.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI, false);
+        jogo.contexto.fillStyle = this.color;
+        jogo.contexto.fill();
+    }
+
+}
+
+class ParticleSystem {
+    constructor(posX, posY, vx, vy, color, maxLife, radius) {
+        for (var i = 0; i < numeroDeParticulas; i++) {
+            if (particleIndex > 1000)
+                particleIndex = 0;
+            particles[particleIndex] = new ParticulaCircular(posX, posY, vx, vy, color, maxLife, radius);
+            particles[particleIndex].id = particleIndex;
+            particleIndex++;
+        }
+    }
+
+    draw() {
+        for (var i in particles)
+            particles[i].draw();
+    }
+}
+
+
+function createFood(posX, posY) {
+    return new ParticleSystem(posX, posY,
+        Math.random() * 10 - 5, Math.random() * 10 - 5,
+        corRGBA(rand(250, 180), rand(250, 180), rand(250, 180), Math.random()),
+        Math.floor(Math.random() * 5), 5);
+}
+
+function createPowerUp(posX, posY) {
+    return new ParticleSystem(posX, posY,
+        Math.random() * 10 - 5, Math.random() * 10 - 5,
+        corRGBA(rand(160, 10), rand(250, 180), rand(160, 10), Math.random()),
+        Math.floor(Math.random() * 5), 5);
+}
+
+function createPowerDown(posX, posY) {
+    return new ParticleSystem(posX, posY,
+        Math.random() * 10 - 5, Math.random() * 10 - 5,
+        corRGBA(rand(250, 180), rand(160, 10), rand(160, 10), Math.random()),
+        Math.floor(Math.random() * 5), 5);
+}
+
+
+function corRGBA(red, green, blue, alpha) {
+    return "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
+}
+
+function corHSLA(hue, saturation, luminosity, alpha) {
+    return "hsla(" + hue + "," + saturation + "," + luminosity + ", " + alpha + ")";
+}
+
+function rand(max, min) {
+    min = min ? min : 0;
+    max = max ? max : 1;
+    return Math.round(Math.random() * max) + min;
+}
+
+function apagaParticula(x, y) {
+    jogo.contexto.fillStyle = "black";
+    jogo.contexto.fillRect(x - 20, y - 20, 40, 40);
+}
--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//link fixe para veres: https://www.youtube.com/watch?v=YCI8uqePkrc
+//https://www.youtube.com/watch?v=YCI8uqePkrc
+var comida = [];
+var powerUps = [];
+var powerDowns = [];
+var CELL = 30;
+
 
 var teclado = {
     LEFT: 37,
@@ -8,7 +13,6 @@ var teclado = {
     SPACE: 32
 };
 
-var CELL = 20;
 var canvas;
 var jogo = {
     contexto: undefined,
@@ -16,16 +20,15 @@ var jogo = {
     height: 0,
     pontuacao: 0,
     emCurso: true,
-    loop: undefined
+    loop: undefined,
+    nivel: 0
 };
-
-var canvas;
 
 var snake = {
     corpo: [],
-    size: 1,
-    direcao: teclado.RIGHT,
-    speed: 3
+    size: 8,
+    cleaner: 2,
+    direcao: teclado.RIGHT
 };
 
 var game = {
@@ -37,39 +40,70 @@ window.addEventListener("load", init);
 
 function init() {
     canvas = document.getElementById("canvas");
+    jogo.width = 1320;
+    jogo.height = 600;
+
+    canvas.width = jogo.width;
+    canvas.height = jogo.height;
     jogo.contexto = canvas.getContext("2d");
-    jogo.width = canvas.width;
-    jogo.height = canvas.height
+
+
+    var posX = [];
+    var posY = [];
     inicioJogo();
+    var posicoesXY = [getPosicoes(), getPosicoes(), getPosicoes()];
+    setInterval(function () {
+        jogo.contexto.fillStyle = "#ffffb8";
+        jogo.contexto.fillRect(0, 0, jogo.width, jogo.height);
+
+        drawBackground();
+
+        comida[0] = createFood(posicoesXY[0].posX, posicoesXY[0].posY);
+        powerUps[0] = (createPowerUp(posicoesXY[1].posX, posicoesXY[1].posY));
+        powerDowns[0] = (createPowerDown(posicoesXY[2].posX, posicoesXY[2].posY));
+
+        //apagaParticula(100, 500);
+        comida[0].draw();
+        powerUps[0].draw();
+        powerDowns[0].draw();
+        renderSnake();
+    }, 60);
+    inicioJogo();
+
+
 }
 
 function inicioJogo() {
-    snake.size = 1;
-    snake.direcao = teclado.RIGHT;
-    snake.speed = 1;
+    for (var i = snake.size - 1; i >= 0; i--)
+        snake.corpo.push({
+            x: i,
+            y: 0
+        });
+    for (var i = snake.cleaner - 1; i >= 0; i--)
+        snake.corpo.push({
+            x: i + snake.size,
+            y: 0
+        });
 
-    snake.corpo = [];
-    for (var i = snake.size - 1; i >= 0; i--) {
-        snake.corpo.push({ x: i, y: 0 });
-    }
-
-    if (!jogo.emCurso) clearInterval(jogo.loop)
-    jogo.loop = setInterval(render, 360 / snake.speed);
+    if (!jogo.emCurso) clearInterval(jogo.loop);
+    jogo.loop = setInterval(render, 120);
     window.addEventListener("keydown", _onKeyDown, false);
 }
 
 function render() {
-    jogo.contexto.fillStyle = "rgba(0,0,0,0.8)";
-    jogo.contexto.fillRect(0, 0, jogo.width, jogo.height);
-    jogo.contexto.strokeStyle = "black";
-    jogo.contexto.strokeRect(0, 0, jogo.width, jogo.height);
-    renderSnake();
+    /*    jogo.contexto.fillStyle = corRGBA(0, 0, 0, 0.2);
+        jogo.contexto.fillRect(0, 0, jogo.width, jogo.height);
+        jogo.contexto.strokeStyle = "black";
+        jogo.contexto.strokeRect(0, 0, jogo.width, jogo.height);
+    */
+    //renderSnake();
 
     //var score_text = "Pontuacao: " + jogo.pontuacao;
     //jogo.contexto.fillText(score_text, 5, 20);
 }
 
 function renderSnake() {
+
     var cabecaX = snake.corpo[0].x;
     var cabecaY = snake.corpo[0].y;
 
@@ -84,22 +118,128 @@ function renderSnake() {
 
     snake.corpo.unshift(cauda);
 
-
     //Desenho
-    for (var i = 0; i < snake.size; i++) {
-        var c = snake.corpo[i];
-        jogo.contexto.fillStyle = "pink";
-        jogo.contexto.fillRect(c.x * CELL, c.y * CELL, CELL, CELL);
-        jogo.contexto.strokeStyle = "black";
-        jogo.contexto.strokeRect(c.x * CELL, c.y * CELL, CELL, CELL);
+
+    //imagem da cabeça
+    var img_head = new Image();
+    img_head.addEventListener('load', function () {}, false);
+    img_head.src = 'head.png';
+  
+    var c;
+    //head
+    var h;
+    for (var i = 1; i < snake.size - 1; i++) {
+        h = snake.corpo[0];
+        c = snake.corpo[i];
+        jogo.contexto.fillStyle = "#00FFFF";
+        jogo.contexto.drawImage(img_head, h.x * (CELL), h.y * (CELL), CELL, CELL);
+        jogo.contexto.drawImage(loadBody(), c.x * (CELL), c.y * (CELL), CELL, CELL);
+        jogo.contexto.strokeStyle = "none";
+        //jogo.contexto.strokeRect(c.x * (CELL), c.y * (CELL), CELL, CELL);
+        console.log("Quadrado " + (i + 1) + ": posicao X: " + c.x + ", posicao Y: " + c.y);
+    }
+
+    for (var i = 0; i < snake.cleaner; i++) {
+        c = snake.corpo[snake.size + i];
+        // jogo.contexto.fillStyle = "rgba(0,0,0,0.5)";
+        //jogo.contexto.fillRect(c.x * (CELL), c.y * (CELL), CELL, CELL);
+
     }
 
 }
 
+function drawBackground() {
+
+    var img = new Image(); // Create new img element
+    img.addEventListener('load', function () {
+        // execute drawImage statements here
+    }, false);
+    img.src = 'back.png';
+    for (var i=0; i < 30; i++) {
+        jogo.contexto.drawImage(img, 0, 50*i, 50, 50);
+        jogo.contexto.drawImage(img, 50*i, 0, 50, 50);
+        jogo.contexto.drawImage(img, canvas.width-50, 50*i, 50, 50);
+        jogo.contexto.drawImage(img, 50*i,canvas.height-50, 50, 50);
+        
+    }
+}
+
+function loadBody() {
+    
+    var n = getRandom(1,12);
+    var img = new Image();
+    img.addEventListener('load', function () {}, false);
+    img.src = 'c' + n + '.png';
+    console.log("random" + n + "//");
+    return img;
+
+}
+
+function getRandom(min, max) {
+     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function _onKeyDown(e) {
+    var novaDirecao;
     var tecla = e.keyCode;
     if (tecla == teclado.DOWN || tecla == teclado.UP || tecla == teclado.LEFT || tecla == teclado.RIGHT)
-        snake.direcao = tecla;
+        novaDirecao = tecla;
+    if (!(snake.direcao == teclado.RIGHT && novaDirecao == teclado.LEFT ||
+            snake.direcao == teclado.LEFT && novaDirecao == teclado.RIGHT ||
+            snake.direcao == teclado.UP && novaDirecao == teclado.DOWN ||
+            snake.direcao == teclado.DOWN && novaDirecao == teclado.UP))
+        snake.direcao = novaDirecao;
     render();
+}
+
+function getPosicoes() {
+    var posX = (rand(Math.round(jogo.width / CELL), 1) - 1) * CELL + CELL / 2;
+    var posY = rand(Math.round(jogo.height / CELL), 1) * CELL + CELL / 2;
+    while (estaOcupado(posX, posY)) {
+        posX = (rand(Math.round(jogo.width / CELL), 1) - 1) * CELL + CELL / 2;
+        posY = rand(Math.round(jogo.height / CELL), 1) * CELL + CELL / 2;
+    }
+    return {
+        posX,
+        posY
+    };
+}
+
+function estaOcupado(x, y) {
+    for (var i = 0; i < comida.length; i++)
+        if (comida[i].x == x && comida[i].y == y)
+            return true;
+    for (var i = 0; i < powerUps.length; i++)
+        if (powerUps[i].x == x && powerUps[i].y == y)
+            return true;
+    for (var i = 0; i < powerDowns.length; i++)
+        if (powerDowns[i].x == x && powerDowns[i].y == y)
+            return true;
+    for (var i = 0; i < snake.size; i++)
+        if (snake.corpo[i].x == x && snake.corpo[i].y == y)
+            return true;
+
+    return false;
+}
+
+function comi() {
+    var x = snake.corpo[0].x;
+    var y = snake.corpo[0].y;
+    for (var i = 0; i < comida.length; i++)
+        if (comida[i].x == x && comida[i].y == y)
+            return camida[i];
+    for (var i = 0; i < powerUps.length; i++)
+        if (powerUps[i].x == x && powerUps[i].y == y)
+            return powerUps[i];
+    for (var i = 0; i < powerDowns.length; i++)
+        if (powerDowns[i].x == x && powerDowns[i].y == y)
+            return powerDowns[i];
+    return undefined;
+}
+
+function snakeOnLimit() {
+    if (snake.corpo[0].x <= 0)
+        snake.corpo[0].x = jogo.width;
+    if (snake.corpo[0].x >= jogo.width)
+        snake.corpo[0].x = 0;
 }
